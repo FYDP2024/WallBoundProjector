5e8edf379c4e418a15c6a1ce9d745b6766c383d7
k35tan <k35tan@uwaterloo.ca>
2024-01-17 13:12:15 -0500

    adding functionality to add previous images to the canvas
    - added prettier extension

--- frontend/src/src/components/CanvasImage.js
+++ frontend/src/src/components/CanvasImage.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-const CanvasImage = ({dimensions}) => {
-    return (
-        <div className="canvas-image"
-            style={{
-                        position: "abosolute",
-                        left: `${dimensions.x_pos}px`,
-                        top: `${dimensions.y_pos}px`,
-                        width: `${dimensions.width}px`,
-                        height: `${dimensions.height}px`,
-                        backgroundImage: `url("${dimensions.url}")`,
-                    }}>
-        </div>
-    );
+const CanvasImage = ({ dimensions }) => {
+  return (
+    <div
+      className="canvas-image"
+      style={{
+        position: "abosolute",
+        left: `${dimensions.x_pos}px`,
+        top: `${dimensions.y_pos}px`,
+        width: `${dimensions.width}px`,
+        height: `${dimensions.height}px`,
+        backgroundImage: `url("imgs/${dimensions.url}")`,
+      }}
+    ></div>
+  );
 };
 
 export default CanvasImage;
