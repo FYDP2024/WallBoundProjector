d3228b0a1460da74d973ea144a7f0cc0d1f04b5e
k35tan <k35tan@uwaterloo.ca>
2024-01-17 21:59:05 -0500

    adding drafts functionality
    - save image of the current canvas
    - save a draft of the canvas as a json file
    - display previous drafts (images of canvas)
    - select previous drafts to go back to

--- frontend/src/src/components/Stats.js
+++ frontend/src/src/components/Stats.js
@@ -1,16 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const Stats = ({}) => {
+const Stats = ({ drafts, loadImagesFromDraft }) => {
   const distance = 10;
   const tilt = [4, 6, 8];
-
+  console.log(drafts);
   return (
-    <div className="sidebar">
+    <div>
       <h3>Info</h3>
       <div>Distance: {distance}cm</div>
       <div>Roll (x): {tilt[0]}°</div>
       <div>Roll (y): {tilt[1]}°</div>
       <div>Roll (z): {tilt[2]}°</div>
+      <h3>Drafts</h3>
+      <div>
+        {drafts.map((draft) => (
+          <>
+            <button onClick={() => loadImagesFromDraft(draft.draft)}>
+              {draft.draft.slice(0, -5)}
+            </button>
+            <div className="image-preview">
+              <div className="prev-row">
+                <img className="thumbnail" src={"drafts/" + draft.image} />
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/src/server.js
+++ frontend/src/src/server.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "../public/imgs"),
@@ -14,10 +16,8 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage });
-
-app.use(express.static(path.join(__dirname, "../public")));
+const draftsDir = path.join(__dirname, "../public/drafts");
 
 app.post("/upload", upload.single("image"), (req, res) => {
   res.json({ message: "Image uploaded successfully" });
@@ -36,12 +36,96 @@ app.get("/getImages", (req, res) => {
           file.endsWith(".jpeg") ||
           file.endsWith(".png")
       );
-      console.log("Images:", imageNames);
       res.json({ images: imageNames });
     }
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const getDraftFiles = () => {
+  return fs
+    .readdirSync(draftsDir)
+    .filter((file) => file.endsWith(".json"))
+    .sort();
+};
+
+const getNextDraftFileName = () => {
+  const draftFiles = getDraftFiles();
+  const lastFile = draftFiles[draftFiles.length - 1];
+  if (!lastFile) {
+    return "Draft_1.json";
+  }
+  const lastNumber = parseInt(lastFile.match(/\d+/)[0]);
+  const nextNumber = lastNumber + 1;
+  return `Draft_${nextNumber}.json`;
+};
+
+app.post("/saveDraft", (req, res) => {
+  const { images } = req.body;
+  const nextFileName = getNextDraftFileName();
+  const jsonString = JSON.stringify(images, null, 2);
+  fs.writeFile(path.join(draftsDir, nextFileName), jsonString, (err) => {
+    if (err) {
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      res.json({ message: "Draft saved successfully", fileName: nextFileName });
+    }
+  });
 });
+
+app.get("/getDrafts", (req, res) => {
+  const draftFiles = getDraftFiles();
+  const draftImages = getDraftImageFiles();
+  if (draftFiles.length === draftFiles.length) {
+    const result = draftFiles.map((draft, index) => ({
+      draft: draft,
+      image: draftImages[index],
+    }));
+    res.json({ drafts: result });
+  } else {
+    res.json({});
+  }
+});
+
+app.get("/getDraft/:fileName", (req, res) => {
+  const { fileName } = req.params;
+  const filePath = path.join(draftsDir, fileName);
+  const data = fs.readFileSync(filePath, "utf-8");
+  const images = JSON.parse(data);
+  res.json({ images });
+});
+
+app.post("/saveCanvasImage", (req, res) => {
+  const { imageData } = req.body;
+  const nextFileName = getNextDraftImageFileName();
+  const filePath = path.join(draftsDir, nextFileName);
+  try {
+    const buffer = Buffer.from(imageData.split(",")[1], "base64");
+    fs.writeFileSync(filePath, buffer);
+    res.json({
+      message: "Canvas image saved successfully",
+      fileName: nextFileName,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const getNextDraftImageFileName = () => {
+  const draftFiles = getDraftImageFiles();
+  const lastFile = draftFiles[draftFiles.length - 1];
+  if (!lastFile) {
+    return "Draft_1.png";
+  }
+  const lastNumber = parseInt(lastFile.match(/\d+/)[0]);
+  const nextNumber = lastNumber + 1;
+  return `Draft_${nextNumber}.png`;
+};
+
+const getDraftImageFiles = () => {
+  return fs
+    .readdirSync(draftsDir)
+    .filter((file) => file.endsWith(".png"))
+    .sort();
+};
+
+app.listen(port, () => {});
