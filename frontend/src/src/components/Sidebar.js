4a58e391d2be2b22e5ba845244c7d61841838aa0
k35tan <k35tan@uwaterloo.ca>
2024-01-25 14:50:14 -0500

    resize/move photo on canvas

--- frontend/src/src/components/Sidebar.js
+++ frontend/src/src/components/Sidebar.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Sidebar = ({ images, deleteCurrentImage }) => {
+const Sidebar = ({ images, deleteCurrentImage, handleEditAndSave }) => {
+  const [editedImages, setEditedImages] = useState([]);
+
+  useEffect(() => {
+    setEditedImages(images);
+  }, [images]);
+
+  const onChange = (image) => {
+    setEditedImages((prevImages) =>
+      prevImages.map((i) => (i.url === image.url ? image : i))
+    );
+  };
+
   return (
     <div>
       <h3>Images</h3>
-      {images.map((img, index) => (
+      {editedImages.map((img, index) => (
         <div className="image-preview" key={index}>
           <button onClick={() => deleteCurrentImage(img.url)}>X</button>
+          <button onClick={() => handleEditAndSave(img)}>
+            {img.isEdit ? "Save" : "Edit"}
+          </button>
           <h3>{index + 1}.</h3>
           <div className="prev-row">
             <img className="thumbnail" src={"imgs/" + img.url} alt={index} />
@@ -15,18 +30,70 @@ const Sidebar = ({ images, deleteCurrentImage }) => {
                 <b>Name: </b>
                 {img.url}
               </div>
-              <div>
-                <b>x position:</b> {img.x_pos}
-              </div>
-              <div>
-                <b>y position:</b> {img.y_pos}
-              </div>
-              <div>
-                <b>Width:</b> {img.width} cm
-              </div>
-              <div>
-                <b>Height:</b> {img.height} cm
-              </div>
+              {img.isEdit && (
+                <>
+                  <div>
+                    <b>x position:</b>
+                    <input
+                      type="number"
+                      name="x_pos"
+                      value={img.x_pos}
+                      onChange={(e) =>
+                        onChange({ ...img, x_pos: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <b>y position:</b>
+                    <input
+                      type="number"
+                      name="y_pos"
+                      value={img.y_pos}
+                      onChange={(e) =>
+                        onChange({ ...img, y_pos: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <b>Width:</b>
+                    <input
+                      type="number"
+                      name="width"
+                      value={img.width}
+                      onChange={(e) =>
+                        onChange({ ...img, width: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div>
+                    <b>Height:</b>
+                    <input
+                      type="number"
+                      name="height"
+                      value={img.height}
+                      onChange={(e) =>
+                        onChange({ ...img, height: e.target.value })
+                      }
+                    />
+                  </div>
+                </>
+              )}
+              {!img.isEdit && (
+                <>
+                  <div>
+                    <b>x position:</b> {img.x_pos}
+                  </div>
+                  <div>
+                    <b>y position:</b> {img.y_pos}
+                  </div>
+                  <div>
+                    <b>Width:</b> {img.width} cm
+                  </div>
+                  <div>
+                    <b>Height:</b> {img.height} cm
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
